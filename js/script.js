9d59a2957a6c7d2df25de962c1b5b986b9c04448
Ernesto Rodriguez <erodrigueztoimil@gmail.com>
2020-02-01 14:19:54 -0500

    working app

--- js/script.js
+++ js/script.js
@@ -1,36 +1,73 @@
 $(document).ready(function() {
-  // handle time
-  var clock = $('.time').text(moment().format("h:mm:ss a"));
+  var currentTime;
+
+  handleCurrentTime(); // display static clock
+  renderTasks(); // render initial tasks and screen
+  renderDoneTask() // handle if the time of the task passed
+
+  function handleCurrentTime() {
+    currentTime = moment().format("H"); // current time in military format
+    $('.time').text(moment().format("h:mm:ss a"));
+  } // display current time
 
   setInterval(function() {
-    clock =  $('.time').text(moment().format("h:mm:ss a"));
+    handleCurrentTime() // render time every second to make clock live
+    renderDoneTask() // handle if the time of the task passed
   }, 1000)
 
-
-  // handle date
-  $('.date').text(moment().format("dddd, MMMM Do YYYY"));
+  $('.date').text(moment().format("dddd, MMMM Do YYYY"));// handle date
 
 
-  // handle schedule
-  var hours = ['9AM', '10AM', '11AM', 'Noon', '1PM', '2PM', '3PM', '4PM', '5PM'];
+  // when executing the function, generate an schedule screen with choosen hours
+  // if saved task display value to corresponding time
+  function renderTasks() {
+    $('ul').empty(); // delete all previous tasks
+    var hours = ['9AM', '10AM', '11AM', 'Noon', '1PM', '2PM', '3PM', '4PM', '5PM']; // initial schedule
+    var savedTask = ''; // default task text
 
-  var savedTask = '';
+    hours.forEach(function(hour, index) {
+      savedTask = localStorage.getItem(hour); // get task corresponding to the time
 
-  hours.forEach(function(hour, index) {
-    savedTask = localStorage.getItem(hour); // get task corresponding to the time
+      if (!savedTask) {
+        savedTask = ''; // if not saved task set to default
+      }
 
-    if (!savedTask) {
-      savedTask = ''; // if saved task = null, dont show anything
-    }
+      mTime = index+9; // change task time to military hour
 
-    // generate an li element for each hour
-    $('ul').append("<li index='"+index+"'><p class='hour'>"+hour+"</p><input class='task-input' value='"+savedTask+"'></input><button class='save-button'>Save</button></li>")
-  });
+      // generate an li element for each hour
+      $('ul').append("<li mTime='"+mTime+"'><p class='hour'>"+hour+"</p><input class='task-input' value='"+savedTask+"'></input><button class='save-button'>Save</button></li>")
+    });
+  }
 
+
+  // when save button is clicked, get user input and save it to local storage
   $('.save-button').click(function() {
     var userInput = $(this).siblings('input').val(); // get user input
     var hour = $(this).siblings('p').text(); // get at what time the user is changing the input
 
-    localStorage.setItem(hour, userInput) // save task with the corresponding hour
+    // if user typed a value then ...
+    if (userInput) {
+      localStorage.setItem(hour, userInput) // save task with the corresponding hour
+    }
+
+    if (!userInput) {
+      localStorage.removeItem(hour); // if the user deletes task, remove it from local storage
+    }
   });
+
+
+  //
+  function renderDoneTask() {
+    var taskTime;
+
+    $('li').each(function() {
+      taskTime = $(this).attr('mTime');
+      taskTime = parseInt(taskTime);
+
+      if (taskTime < currentTime) {
+        $(this).css('background-color', 'red');
+        // renderTasks()
+      }
+    });
+  }
 });
